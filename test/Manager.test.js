8fcf3ad4013656ba668fa8922f2aef4fab2cfe4a
SirPotatoIV <otoolej33@gmail.com>
2019-12-11 19:33:59 -0600

    manager.js passing all tests. Added manager.test.js to complete said testing. Changed name of intern.test.js to stop it from running.

---
+++ test/Manager.test.js
@@ -0,0 +1,21 @@
+const Manager = require("../lib/Manager");
+const Employee = require("../lib/Employee");
+//Passing
+test("Can set office number via constructor argument", () => {
+  const testValue = 100;
+  const e = new Manager("Foo", 1, "test@test.com", testValue);
+  expect(e.officeNumber).toBe(testValue);
+});
+//Passing
+test("getRole() should return \"Manager\"", () => {
+  const testValue = "Manager";
+  const e = new Manager("Foo", 1, "test@test.com", 100);
+  expect(e.getRole()).toBe(testValue);
+});
+//Passing
+// -- So awesome that the test says getOffice(), but it's testing for getOfficeNumber() .....
+test("Can get office number via getOffice()", () => {
+  const testValue = 100;
+  const e = new Manager("Foo", 1, "test@test.com", testValue);
+  expect(e.getOfficeNumber()).toBe(testValue);
+});
