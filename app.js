8f6a56bf3ed58f0d9c3ea1c5618b904c5c5f5dcb
SirPotatoIV <otoolej33@gmail.com>
2019-12-12 19:11:56 -0600

    Successfully created a manager.

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
+const inquirer = require('inquirer');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
 // The project must prompt the user to build an engineering team. An engineering
 // team consists of a manager, and any number of engineers and interns.
 
 // The project must generate a `team.html` page in the `output` directory, that displays a nicely formatted team roster. Each team member should display the following in no particular order:
-
-//   * Name
-
-//   * Role
-
-//   * ID
-
-//   * Role-specific property (School, link to GitHub profile, or office number)
+const team = [];
+function createTeam(){
+    const manager = new Manager;
+    console.log(manager);
+}
+createTeam();
